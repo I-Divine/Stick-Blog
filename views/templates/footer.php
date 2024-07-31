233f8b426cffdd1692a64bc2984f92b0521d8c17
Igwedinma Ifechukwu Divine <64442300+I-Divine@users.noreply.github.com>
2024-07-31 23:30:41 +0100

    Created templates

---
+++ views/templates/footer.php
@@ -0,0 +1,5 @@
+<footer class=" p-12">
+<p class=" text-center text-neutral-500 text-xl">Free to use </p>
+</footer>
+</body>
+</html>
---
+++ views/templates/head.php
@@ -0,0 +1,17 @@
+<html>  
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>The Sticks| The Blog</title>
+    </head>
+    <body>
+        <header class="p-8 flex justify-between border-b border-red-400 ">
+            <h1 class="text-3xl text-red-600">Sticks Blog</h1>
+            <nav>
+                <a href="" class="p-4 text-white bg-red-500">Add blog</a>
+                <!-- <a href=""></a>
+                <a href=""></a> -->
+            </nav>
+        </header>    
+    
